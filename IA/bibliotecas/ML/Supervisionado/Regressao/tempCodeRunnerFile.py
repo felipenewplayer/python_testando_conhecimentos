8901f0c1738d1955885f8936219055be748cbce7
felipenewplayer <felipeapereira2014@gmail.com>
2025-11-09 11:51:02 -0300

    mais_estudos

---
+++ IA/bibliotecas/ML/Supervisionado/Regressao/tempCodeRunnerFile.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model  import LinearRegression
+
+altura = np.array([130,150,160,170,170,170]).reshape(-1,1) # matriz
+idade = np.array([15,17,19,21,23,25])
+
+dados = ({
+    'Altura':altura,
+    'Idade':idade
+})
+
+df = pd.DataFrame(dados)
+print(df)
+
+modelo = LinearRegression()
+modelo.fit(altura,idade)
+predicao = modelo.predict([[180]])
+print(f'Idade previta {predicao}')
+
+sns.barplot(x='Altura', y='Idade',data=df, color='purple')
+plt.title("Relação entre Altura e Idade")
+plt.show()
+
--- IA/bibliotecas/Pandas/ia_com_pandas.py
+++ IA/bibliotecas/Pandas/ia_com_pandas.py
@@ -1,9 +1,85 @@
 import pandas as pd
-dados = pd.read_excel("C:/Users/felip\OneDrive/Área de Trabalho/Felipe/TI/Programação/PHYTON/Python2025/OpensXl/arquivo1.xlsx")
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+# Lendo os dados
+dados = pd.read_csv(
+    r'C:\Users\felip\OneDrive\Área de Trabalho\Felipe\TI\Programação\PHYTON\Python2025\IA\bibliotecas\Pandas\athlete_events.csv'
+)
+
+# Criando cópia e removendo colunas desnecessárias
+filtro = dados.drop(columns=['Sport', 'Event']).copy()
+
+# Preenchendo valores faltantes com a média das colunas
+filtro[['Age', 'Height', 'Weight']] = filtro[['Age', 'Height', 'Weight']].fillna(
+    filtro[['Age', 'Height', 'Weight']].mean()
+)
 
 
-dados2 = pd.read_csv(r"C:\Users\felip\OneDrive\Área de Trabalho\Felipe\TI\Programação\PHYTON\Python2025\OpensXl\athlete_events.csv")
+# Arredondando as colunas numéricas
+filtro[['Height', 'Weight']] = filtro[['Height', 'Weight']].round(2)
+filtro['Age'] = filtro['Age'].round(0).astype(int)
 
-print(dados.head(7))
-print(dados2.head(5))
+# Exibindo as 5 primeiras linhas
+print(filtro.head(5))
+
+
+X_variable = filtro.loc[:, ['Height','Weight','Year']].values
+Y_variable = filtro.loc[:, 'Age'].values
+
+print(f'\nVariável Y : \n{Y_variable}')
+print(f'\nVariável X : \n{X_variable}')
+print(f'\nTipo de dados : {type(X_variable)}')
+
+
+scaler = StandardScaler()
+x_sclaler =scaler.fit_transform(X_variable)
+
+modelo = LinearRegression()
+modelo.fit(x_sclaler, Y_variable)
+
+print("Coeficientes:", modelo.coef_)
+print("Intercepto:", modelo.intercept_)
+
+nova_entrada = scaler.transform([[180,75,2001]])
+predicao = modelo.predict(nova_entrada).astype(int)
+print(f'Previsão de idade: {predicao[0]}')
+r2 = modelo.score(x_sclaler, Y_variable)
+print("R² do modelo:", r2)
+
+scaler_altura= x_sclaler[:,0].min()
+scaler_peso = x_sclaler[:,1].min()
+scaler_ano = x_sclaler[:,2].min()
+print(scaler_altura,scaler_peso,scaler_ano)
+
+
+
+
+# medalhas_por_pais = dados.loc[dados['Medal'].notna()].groupby('Team')['Medal'].count().sort_values(ascending=False)
+# print(medalhas_por_pais.head(5))
+
+# medalhas_ano = dados.loc[dados['Medal'].notna()].groupby('Year')['Medal'].count().sort_values(ascending=False)
+# print(medalhas_ano)
+
+# tabela = dados.loc[dados['Medal'].notna()].pivot_table(
+#     index='Team',
+#     columns='Medal',
+#     values='Name',
+#     aggfunc='count',
+#     fill_value=0
+# )
+
+# print(tabela.head(10))
+
+
+# grafico = px.bar(
+#     medalhas_por_pais.head(10).reset_index(),
+#     x='Team',
+#     y='Medal',
+#     title='Top 10 Países com Mais Medalhas',
+#     color='Team'
+# )
+# grafico.show()
+
+
+
 

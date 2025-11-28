e9d1a60b76954f64f5e290690f08b0859674279a
felipenewplayer <felipeapereira2014@gmail.com>
2025-11-27 21:08:03 -0300

    iniciando regressao

---
+++ IA/ML/Supervisionado/Regressao/Base_plano_saude/regressao_linear.py
@@ -0,0 +1,9 @@
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+base_plano_saude = pd.read_csv(r'C:\Users\felip\OneDrive\Área de Trabalho\Felipe\TI\Programação\PHYTON\Python2025\IA\ML\Supervisionado\Regressao\Base_plano_saude\plano_saude.csv')
+
+print(base_plano_saude.head())

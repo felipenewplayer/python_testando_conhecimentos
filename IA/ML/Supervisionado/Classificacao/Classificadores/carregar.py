8a4db550ef74fbd3053d114cc9d56e362dcec374
felipenewplayer <felipeapereira2014@gmail.com>
2025-12-04 21:16:31 -0300

    carregando_classificadores

---
+++ IA/ML/Supervisionado/Classificacao/Classificadores/carregar.py
@@ -0,0 +1,8 @@
+import pickle
+
+
+rede_neural = pickle.load(open('IA/ML/Supervisionado/Classificacao/Classificadores/rede_neural_classificador.sav', 'rb'))
+svm = pickle.load(open('IA/ML/Supervisionado/Classificacao/Classificadores/svm_classificador.sav', 'rb'))
+arvore_decisao = pickle.load(open('IA/ML/Supervisionado/Classificacao/Classificadores/arvore_decisao_classificador.sav', 'rb'))
+
+

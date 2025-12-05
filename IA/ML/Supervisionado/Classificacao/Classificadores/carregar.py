77b540884318f5cd23aca67d213f57c0465e0973
felipenewplayer <felipeapereira2014@gmail.com>
2025-12-04 21:21:43 -0300

    carregando_classificadores1.1

--- IA/ML/Supervisionado/Classificacao/Classificadores/carregar.py
+++ IA/ML/Supervisionado/Classificacao/Classificadores/carregar.py
@@ -1,8 +1,14 @@
 import pickle
+import numpy as np
 
+with open(r'C:\Users\felip\OneDrive\Área de Trabalho\Felipe\TI\Programação\PHYTON\Python2025\IA\ML\Supervisionado\Classificacao\PreProcessamento\Base_credit\credit.pkl', 'rb')as f:
+    X_credit_treinamento, X_credit_teste, y_credit_treinamento, y_credit_teste = pickle.load(f)
+    
 
-rede_neural = pickle.load(open('IA/ML/Supervisionado/Classificacao/Classificadores/rede_neural_classificador.sav', 'rb'))
-svm = pickle.load(open('IA/ML/Supervisionado/Classificacao/Classificadores/svm_classificador.sav', 'rb'))
-arvore_decisao = pickle.load(open('IA/ML/Supervisionado/Classificacao/Classificadores/arvore_decisao_classificador.sav', 'rb'))
+X_credit = np.concatenate((X_credit_treinamento, X_credit_teste), axis=0)
+y_credit = np.concatenate((y_credit_treinamento, y_credit_teste), axis=0)
 
+rede_neural = pickle.load(open(r'C:\Users\felip\OneDrive\Área de Trabalho\Felipe\TI\Programação\PHYTON\Python2025\IA\ML\Supervisionado\Classificacao\Classificadores\Classificadores_Salvos\rede_neural_finalizado.sav', 'rb'))
+svm = pickle.load(open(r'C:\Users\felip\OneDrive\Área de Trabalho\Felipe\TI\Programação\PHYTON\Python2025\IA\ML\Supervisionado\Classificacao\Classificadores\Classificadores_Salvos\svm_finalizado.sav', 'rb'))
+arvore_decisao = pickle.load(open(r'C:\Users\felip\OneDrive\Área de Trabalho\Felipe\TI\Programação\PHYTON\Python2025\IA\ML\Supervisionado\Classificacao\Classificadores\Classificadores_Salvos\arvore_decisao_finalizado.sav', 'rb'))
 

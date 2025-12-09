594143a4dad1bee5365774431d0d68b24a0d7db8
felipenewplayer <felipeapereira2014@gmail.com>
2025-12-09 05:35:46 -0300

    marcação POS

--- IA/PNL/app.py
+++ IA/PNL/app.py
@@ -3,3 +3,13 @@ import urllib.request
 import nltk
 import spacy
 
+
+# POS (part-of-speech) atribui para cada palavra de um texto sua classe gramatical
+# Exemplo: substantivo, verbo, adjetivo, advérbio, pronome
+pnl = spacy.load("pt_core_news_sm")
+print(pnl)
+
+documento = pnl("Estou aprendendo processamento de linguagem natural, em São Paulo.")
+
+for token in documento:
+    print(token.text, token.pos_)
